6b23d54e96e1132e238bd3a7d826f0471ced54b2
S-Axhwin <s.ashwin.sathiyamoorthy.d@gmail.com>
2024-03-12 11:56:20 +0530

    base

--- src/components/auth/Axios.js
+++ src/components/auth/Axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://server-production-fa75.up.railway.app/",
+    baseURL: "https://server-production-fa75.up.railway.app",
+
 })
